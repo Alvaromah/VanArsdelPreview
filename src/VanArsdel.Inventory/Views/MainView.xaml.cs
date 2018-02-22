0ce0cc34ed5cf65a8fa2550e8b47e90e1e21b535
Alvaro Mateos <alvaroma@live.com>
2018-02-22 08:06:18 +0100

    ViewManager and MainView.

---
+++ src/VanArsdel.Inventory/Views/MainView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+using VanArsdel.Inventory.Data;
+
+namespace VanArsdel.Inventory.Views
+{
+    public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        #region IsBusy
+        public bool IsBusy
+        {
+            get { return (bool)GetValue(IsBusyProperty); }
+            set { SetValue(IsBusyProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsBusyProperty = DependencyProperty.Register("IsBusy", typeof(bool), typeof(MainView), new PropertyMetadata(false));
+        #endregion
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            IsBusy = true;
+            await Task.Delay(100);
+            await DataHelper.Current.InitializeAsync(new DataProviderFactory());
+            if (e.Parameter is MainViewState state)
+            {
+                frame.Navigate(state.PageType, state.Parameter);
+            }
+            IsBusy = false;
+        }
+    }
+
+    public class MainViewState
+    {
+        public Type PageType { get; set; }
+        public object Parameter { get; set; }
+    }
+}
---
+++ src/VanArsdel.Inventory/Views/ViewManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.ViewManagement;
+using Windows.UI.Core;
+using Windows.ApplicationModel.Core;
+
+namespace VanArsdel.Inventory.Views
+{
+    public class ViewManager
+    {
+        static public ViewManager Current { get; }
+
+        static ViewManager()
+        {
+            Current = new ViewManager();
+        }
+
+        private ViewManager()
+        {
+        }
+
+        public async Task<int> CreateNewView(Type pageType, object parameter = null)
+        {
+            int viewId = 0;
+
+            CoreApplicationView newView = CoreApplication.CreateNewView();
+            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                var state = new MainViewState
+                {
+                    PageType = pageType,
+                    Parameter = parameter
+                };
+
+                var frame = new Frame();
+                frame.Navigate(typeof(MainView), state);
+
+                Window.Current.Content = frame;
+                Window.Current.Activate();
+
+                viewId = ApplicationView.GetForCurrentView().Id;
+            });
+
+            if (await ApplicationViewSwitcher.TryShowAsStandaloneAsync(viewId))
+            {
+                return viewId;
+            }
+
+            return 0;
+        }
+    }
+}
