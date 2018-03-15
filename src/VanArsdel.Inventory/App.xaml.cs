9c9b880fc5ccac5d837669aaf71aef9661ad95cf
Alvaro Mateos <alvaroma@live.com>
2018-03-15 14:54:32 +0100

    Customer card in details.

--- src/VanArsdel.Inventory/App.xaml.cs
+++ src/VanArsdel.Inventory/App.xaml.cs
@@ -42,7 +42,7 @@ namespace VanArsdel.Inventory
                     var state = new MainViewState
                     {
                         PageType = EntryPage,
-                        Parameter = new ShellViewState { Current = KnownNavigationItems.Dashboard }
+                        Parameter = new ShellViewState { Current = KnownNavigationItems.Customers }
                     };
                     frame.Navigate(typeof(MainView), state);
                 }
