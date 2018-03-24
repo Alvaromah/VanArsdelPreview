555cf57368bcd58b572ead8d5123d6041c7440df
Alvaro Mateos <alvaroma@live.com>
2018-03-24 08:57:44 +0100

    Dialog Service.

--- src/VanArsdel.Inventory/Configuration/Startup.cs
+++ src/VanArsdel.Inventory/Configuration/Startup.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
+
+using Windows.Storage;
 
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,8 +19,6 @@ namespace VanArsdel.Inventory
 
         static public async Task ConfigureAsync()
         {
-            await Task.Delay(500);
-
             ServiceLocator.Configure(_serviceCollection);
 
             NavigationService.Register<ShellViewModel, ShellView>();
@@ -26,6 +28,33 @@ namespace VanArsdel.Inventory
             NavigationService.Register<OrdersViewModel, OrdersView>();
             NavigationService.Register<ProductsViewModel, ProductsView>();
             NavigationService.Register<SettingsViewModel, SettingsView>();
+
+            await EnsureDatabaseAsync();
+        }
+
+        static private async Task EnsureDatabaseAsync()
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var databaseFolder = await localFolder.CreateFolderAsync(AppSettings.DatabasePath, CreationCollisionOption.OpenIfExists);
+
+            if (await databaseFolder.TryGetItemAsync(AppSettings.DatabaseName) == null)
+            {
+                if (await databaseFolder.TryGetItemAsync(AppSettings.DatabasePattern) == null)
+                {
+                    using (var cli = new WebClient())
+                    {
+                        var bytes = cli.DownloadData(AppSettings.DatabaseUrl);
+                        var file = await databaseFolder.CreateFileAsync(AppSettings.DatabasePattern, CreationCollisionOption.ReplaceExisting);
+                        using (var stream = await file.OpenStreamForWriteAsync())
+                        {
+                            await stream.WriteAsync(bytes, 0, bytes.Length);
+                        }
+                    }
+                }
+                var sourceFile = await databaseFolder.GetFileAsync(AppSettings.DatabasePattern);
+                var targetFile = await databaseFolder.CreateFileAsync(AppSettings.DatabaseName, CreationCollisionOption.ReplaceExisting);
+                await sourceFile.CopyAndReplaceAsync(targetFile);
+            }
         }
     }
 }
--- src/VanArsdel.Inventory/Services/DialogService/DialogService.cs
+++ src/VanArsdel.Inventory/Services/DialogService/DialogService.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace VanArsdel.Inventory.Services
 {
     public interface IDialogService
     {
+        Task ShowAsync(string title, Exception ex, string ok = "Ok");
+        Task ShowAsync(Result result, string ok = "Ok");
+
+        Task<bool> ShowAsync(string title, string content, string ok = "Ok", string cancel = null);
     }
 }
