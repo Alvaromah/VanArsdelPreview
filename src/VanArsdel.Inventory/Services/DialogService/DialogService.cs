7186038478157d1d707405570828a1b56ed920a3
Alvaro Mateos <alvaroma@live.com>
2018-04-09 21:33:23 +0200

    Switch DialogService and IDialogService.

--- src/VanArsdel.Inventory/Services/DialogService/DialogService.cs
+++ src/VanArsdel.Inventory/Services/DialogService/DialogService.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Threading.Tasks;
 
+using Windows.UI.Xaml.Controls;
+
 namespace VanArsdel.Inventory.Services
 {
-    public interface IDialogService
+    public class DialogService : IDialogService
     {
-        Task ShowAsync(string title, Exception ex, string ok = "Ok");
-        Task ShowAsync(Result result, string ok = "Ok");
+        public async Task ShowAsync(string title, Exception ex, string ok = "Ok")
+        {
+            await ShowAsync(title, ex.Message, ok);
+        }
 
-        Task<bool> ShowAsync(string title, string content, string ok = "Ok", string cancel = null);
+        public async Task ShowAsync(Result result, string ok = "Ok")
+        {
+            await ShowAsync(result.Message, result.Description, ok);
+        }
+
+        public async Task<bool> ShowAsync(string title, string content, string ok = "Ok", string cancel = null)
+        {
+            var dialog = new ContentDialog
+            {
+                Title = title,
+                Content = content,
+                PrimaryButtonText = ok
+            };
+            if (cancel != null)
+            {
+                dialog.SecondaryButtonText = cancel;
+            }
+            var result = await dialog.ShowAsync();
+            return result == ContentDialogResult.Primary;
+        }
     }
 }
--- src/VanArsdel.Inventory/Services/DialogService/IDialogService.cs
+++ src/VanArsdel.Inventory/Services/DialogService/IDialogService.cs
@@ -1,36 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-using Windows.UI.Xaml.Controls;
-
 namespace VanArsdel.Inventory.Services
 {
-    public class DialogService : IDialogService
+    public interface IDialogService
     {
-        public async Task ShowAsync(string title, Exception ex, string ok = "Ok")
-        {
-            await ShowAsync(title, ex.Message, ok);
-        }
+        Task ShowAsync(string title, Exception ex, string ok = "Ok");
+        Task ShowAsync(Result result, string ok = "Ok");
 
-        public async Task ShowAsync(Result result, string ok = "Ok")
-        {
-            await ShowAsync(result.Message, result.Description, ok);
-        }
-
-        public async Task<bool> ShowAsync(string title, string content, string ok = "Ok", string cancel = null)
-        {
-            var dialog = new ContentDialog
-            {
-                Title = title,
-                Content = content,
-                PrimaryButtonText = ok
-            };
-            if (cancel != null)
-            {
-                dialog.SecondaryButtonText = cancel;
-            }
-            var result = await dialog.ShowAsync();
-            return result == ContentDialogResult.Primary;
-        }
+        Task<bool> ShowAsync(string title, string content, string ok = "Ok", string cancel = null);
     }
 }
